c8c25ddc37776ffc16d861d4a5e7cd99c92b15ec
WolfyThunder <Wldegrafft@email.neit.edu>
2024-10-24 08:59:34 -0400

    loops

---
+++ w4/w4d2.js
@@ -0,0 +1,19 @@
+var name = `Katie`
+
+var htmlNames = [
+    `Loki`,       // 0
+    `Abigail` ,   // 1
+    `Andrew`      // 2
+]
+
+console.log(`INDEX 0: ${htmlNames[0]}`)
+
+htmlNames.push(`Wolfy`)
+
+htmlNames[4] = `Max`
+
+for(var i = 0; i< htmlNames.length; i++){
+
+    console.log(`INDEX ${i}: ${htmlNames[i]}`)
+
+}
